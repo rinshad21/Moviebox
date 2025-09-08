d5db5f3f5a63debc7250628f20ca5245e814b364
Rinshad KA <mdrinshadka@gmail.com>
2025-09-08 20:06:43 +0530

    shadcn update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import MovieCard from "../components/MovieCard";
 import { useState, useEffect } from "react";
 import { getPopularMovies, searchMovies } from "../services/api";
+import { Search } from "lucide-react";
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 function Home() {
   //intialized Search Query store user input
@@ -48,20 +51,31 @@ function Home() {
   };
 
   return (
+    //search bar 
     <div>
       <div className=" flex items-center ">
-        <form onSubmit={handleSearch}>
-          <input
-            className="bg-gray-800 rounded p-2 lg:w-100 mt-5 w-50 border-0 ml-18 lg:ml-110"
-            type="text"
-            value={searchQuery}
-            placeholder="search for movies"
-            onChange={(e) => setSearchQuery(e.target.value)}
-          />
-          <button className="bg-red-800 p-2 ml-2 rounded" type={"submit"}>
-            Search
-          </button>
-        </form>
+        <form onSubmit={handleSearch} className="flex items-center gap-2 mt-5">
+    <Input
+      className="bg-gray-800 border-blue-400/50 active:border-blue-400/70 
+                 focus:ring-2 focus:ring-blue-400/50 outline-none 
+                 transition duration-200 rounded-xl p-2 w-66 ml-7 lg:ml-120 lg:w-96 border-0"
+      type="text" 
+      value={searchQuery}
+      placeholder="Search for movies"
+      onChange={(e) => setSearchQuery(e.target.value)}
+    />
+
+    <Button
+      type="submit"
+      className="flex items-center gap-2 bg-gradient-to-r from-red-700 to-red-800 
+                 active:from-red-600 active:to-red-700 text-white 
+                 px-4 py-2 rounded-xl shadow-md border border-red-900/40 
+                 transition duration-200 "
+    >
+      <Search size={16} className="text-white" />
+      <span className="hidden sm:inline">Search</span>
+    </Button>
+  </form>
       </div>
       <h3 className="font-semibold text-2xl lg:mt-2 mt-5 lg:ml-9 ml-17">
         Popular this week
@@ -77,7 +91,7 @@ function Home() {
             <MovieCard
               key={movie.id}
               movie={movie}
-            />
+            /> 
           ))}
         </div>
       )}
